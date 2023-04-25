cbaf2897c7eca1167180b4bbc8203b1b1f719572
Killian <paulic.killian@gmail.com>
2023-04-25 18:52:03 +0200

    articleCard done with the data

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { ArticleCard } from "../../components/articleCard/articleCard";
 import "./home.css";
+import { articlesList } from "../../data/articles";
 
 export const Home = () => {
   return (
     <div>
       <h1 className="page-title">Home</h1>
-      <ArticleCard />
+      <div className="article-gallery">
+        {articlesList.map((article) => (
+          <ArticleCard key={article.id} {...article} />
+        ))}
+      </div>
     </div>
   );
-}
+};
